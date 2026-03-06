aab0213364ddbeabb57923dd1ce9add88837376a
vereesmort <kkatcchya@gmail.com>
2026-03-06 14:19:32 +0100

    Exclude ppi dataset

--- Datasets/download_and_unpack.sh
+++ Datasets/download_and_unpack.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-wget http://snap.stanford.edu/decagon/bio-decagon-ppi.tar.gz
+# wget http://snap.stanford.edu/decagon/bio-decagon-ppi.tar.gz -- This is unusued in this project
 # wget http://snap.stanford.edu/decagon/bio-decagon-targets.tar.gz
 
 wget http://snap.stanford.edu/decagon/bio-decagon-targets-all.tar.gz
 
 wget http://snap.stanford.edu/decagon/bio-decagon-combo.tar.gz
 wget http://snap.stanford.edu/decagon/bio-decagon-mono.tar.gz
-#wget http://snap.stanford.edu/decagon/bio-decagon-effectcategories.tar.gz -- This is unusued in this analysis
+#wget http://snap.stanford.edu/decagon/bio-decagon-effectcategories.tar.gz -- This is unusued in this project
 
 for f in *.gz;
 do
